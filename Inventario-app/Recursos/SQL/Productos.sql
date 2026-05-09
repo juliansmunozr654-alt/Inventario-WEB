607d7c8caeaa5fc21e846027b6ef081115232f26
juliansmunozr654-alt <juliansmunozr654@gmail.com>
2026-05-09 10:32:11 -0500

    Delete Inventario-app directory

--- Inventario-app/Recursos/SQL/Productos.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE productos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100),
-    marca VARCHAR(100),
-    precio DECIMAL(10,2),
-    cantidad INT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
